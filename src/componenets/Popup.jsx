498787a971d3ee16ba055d3b6a0aabd981b5f047
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2022-11-29 17:31:42 +1300

    better positioning and random images

--- src/componenets/Popup.jsx
+++ src/componenets/Popup.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react'
 
 export default function Popup() {
-  const [index, setIndex] = useState(0.33)
+  const [index, setIndex] = useState(null)
   const [pop, setPop] = useState(false)
-  const image = 'image.jpeg'
-  let size = Math.floor(Math.random() * 1000)
+  const image = [
+    'image.jpeg',
+    'macpop.png',
+    'pop.jpeg',
+    'pop2.png',
+    'winodwpop.png',
+  ]
+  let size = Math.floor(Math.random() * 1000 + 300)
   // const popUpStyle = { color: 'red', width: '200px', margin: '5px' }
   function popup(e) {
     // e.preventdefault()
     setPop(true)
-    setIndex(Math.floor(Math.random() * 100))
+    setIndex(Math.floor(Math.random() * 4))
   }
 
   return (
     <>
       {pop ? (
         <div className="popUp">
-          <img src={image} alt="popup ad" style={{ width: size }}></img>
+          <img src={image[index]} alt="popup ad" style={{ width: size }}></img>
           <button
             onClick={() => {
               setPop(false)
